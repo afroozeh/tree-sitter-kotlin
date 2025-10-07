7e5346f7a3bd123d27b9ff2f39f1e3f86c5dd9ee
Ali Afroozeh <afroozeh@gmail.com>
2025-10-07 23:05:59 +0200

    Update README (#56)
    Updates README.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -366,9 +366,11 @@
 
 ; NOTE: `interpolated_identifier`s can be highlighted in any way
 (string_literal
-	"$" @punctuation.special
-	(interpolated_identifier) @none)
+	(string_content
+		"$" @punctuation.special
+		(interpolated_identifier) @none))
 (string_literal
-	"${" @punctuation.special
-	(interpolated_expression) @none
-	"}" @punctuation.special)
+	(string_content
+		"${" @punctuation.special
+		(interpolated_expression) @none
+		"}" @punctuation.special))
